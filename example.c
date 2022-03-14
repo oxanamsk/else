971dbd3bbcf770f33c8279cc5cf82806f3f61b41
oxanamsk <oxanamirzoeva@gmail.com>
2022-03-14 21:47:44 +0300

    calcuneus

---
+++ example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int x=8, y=16, res;
+    x--;
+    y++;
+    y*=15;
+    res=x+y;
+printf("%d", res);
+    return 0;
+}
