cd38e3b16973671e51fb28eb5354c7add23ac14f
oxanamsk <oxanamirzoeva@gmail.com>
2022-03-16 18:48:44 +0300

    array1

--- array.c
+++ array.c
@@ -5,6 +5,27 @@
 int main () {
 int arr [5] = {5, 16, 32, 24, 87};
 arr [0];
-printf ("%d", arr[0]);
+printf ("%d\n", arr[0]);
+
+float numbers [3];
+    numbers [0] = 6.4f;
+    numbers [1] = 7.1f;
+    numbers [2] = 9.8f;
+
+    char word [] = {'S', 'o', 'm', 'e'};
+    char words [] = "Hello World!";
+
+    words [1] = 'G';
+    printf("%s\n", words);
+    printf("%c\n", words[0]);
+
+
+    int array [3][4] = {
+        {4, 5, 6,124},
+        {1, 5, 7 ,12},
+        {3, 6, 9, 13}
+    };
+        array[1][1] = 9;
+        printf("%d\n", array[0][0]);
 return 0;
 }
