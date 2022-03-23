c8c77f6667a53200ca429286b71e9d2a4441fca7
oxanamsk <oxanamirzoeva@gmail.com>
2022-03-23 18:34:24 +0300

    newelseif2

--- newelseif.c
+++ newelseif.c
@@ -3,11 +3,15 @@
 #include <unistd.h>
 
 int main () {
-    int x=107;
-    if (x ==10) {
-        printf ("Yes\n");
+    int x=5;
+        if (x==10) {
+            printf("Yes\n");
         } else if (x > 100) {
-        printf ("X is greatest 100\n");
+            printf("X is greatest 100\n");
+        } else if (x < 5) {
+            printf("X is less than 5\n");
+        } else {
+            printf("No\n");
         }
-    return 0;
+        return 0;
 }
