b112847ec519729e3e32b7a1d6eb0d5c31ae9601
oxanamsk <oxanamirzoeva@gmail.com>
2022-03-23 17:04:40 +0300

    elseif

---
+++ newelseif.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main () {
+    int x=107;
+    if (x ==10) {
+        printf ("Yes\n");
+        } else if (x > 100) {
+        printf ("X is greatest 100\n");
+        }
+    return 0;
+}
