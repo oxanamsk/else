c9d2e10faca0e016df47f83dfa58d74862fe55b4
oxanamsk <oxanamirzoeva@gmail.com>
2022-03-23 18:58:09 +0300

    less

--- newelseif.c
+++ newelseif.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+
 
 int main () {
-    int x=5;
-        if (x==10) {
-            printf("Yes\n");
-        } else if (x > 100) {
-            printf("X is greatest 100\n");
-        } else if (x < 5) {
-            printf("X is less than 5\n");
-        } else {
-            printf("No\n");
-        }
-        return 0;
+    int x=20, y=5;
+     if ((x==20&& y<7) || x==20)
+        printf("YES\n");
+        else if (x > 100) {
+            if (x== 105)
+            printf ("X is equal 100\n");
+
+            printf ("X is greater 100\n");
+        } else if (x < 5)
+        } printf ("X is less than 5\n");
+        else
+            printf ("No\n");
+return 0;
 }
