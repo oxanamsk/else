1b9b2759df50c093a1a5e391fe5fad10f7c36770
oxanamsk <oxanamirzoeva@gmail.com>
2022-03-23 15:14:23 +0300

    marchelse

---
+++ else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main () {
+    int x=5;
+        if (x==5){
+            printf ("Hello Evgeniy!");
+        }
+    return 0;
+}
